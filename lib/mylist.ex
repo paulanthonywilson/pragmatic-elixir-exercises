c9849d1392993705fe34b3417a3aac774dbcfc76
Paul Wilson <paulanthonywilson@gmail.com>
2013-08-25 11:50:14 +0100

    to flatten

--- lib/mylist.ex
+++ lib/mylist.ex
@@ -13,4 +13,24 @@ defmodule MyList do
   def filter(list = [h|_], fun), do: filter(list, fun, fun.(h))
   def filter([h|t], fun, true), do: [h | filter(t,fun)]
   def filter([_|t], fun, false), do: filter(t,fun)
+
+
+  def split(list,n) when n < 0 do
+    {l,r} = split(Enum.reverse(list), -n)
+    {Enum.reverse(r), Enum.reverse(l)}
+  end
+
+  def split(list, n), do: _split([], list, n)
+
+  defp _split(l, r, 0), do: {l,r}
+  defp _split(l, [], _), do: {l,[]}
+  defp _split(l,[h|t], n), do: _split(l ++ [h], t, n-1)
+
+
+  def flatten([]), do: []
+  def flatten([h|t]) when is_list(h), do: flatten(h) ++ flatten(t)
+  def flatten([h|t]), do: [h] ++ flatten(t)
+
 end
+
+
--- test/mylist_test.exs
+++ test/mylist_test.exs
@@ -3,9 +3,10 @@ Code.require_file "test_helper.exs", __DIR__
 defmodule MyListTest do
   use ExUnit.Case
 
+  # This should be a Macro, but I haven't done those yet :-/
   def assert_equiv_fun function_under_test_atom do
       under5 = fn n -> n < 5 end
-      fn list ->
+      fn(list) ->
         assert apply(MyList, function_under_test_atom, [list, under5]) ==
                apply(Enum, function_under_test_atom, [list, under5])
       end
@@ -38,6 +39,44 @@ defmodule MyListTest do
     assert MyList.each([1, 2], IO.puts(&1)) == :ok
   end
 
+  test "split" do
+    assert_equiv = fn(list, n)-> assert MyList.split(list, n) == Enum.split(list, n) end
+    assert_equiv.([],0)
+    assert_equiv.([],1)
+
+
+    assert_equiv.([1,2,3],0)
+    assert_equiv.([1,2,3],1)
+    assert_equiv.([1,2,3],2)
+    assert_equiv.([1,2,3],3)
+    assert_equiv.([1,2],3)
+
+
+    assert_equiv.([],-1)
+
+
+    assert_equiv.([1,2,3],-1)
+    assert_equiv.([1,2,3],-2)
+    assert_equiv.([1,2,3],-3)
+    assert_equiv.([1,2],-3)
+
+
+    assert_equiv.([1,2,3, 4, 5],-2)
+
+  end
+
+
+  test "flatten" do
+    assert_equiv = fn(list)-> assert MyList.flatten(list) == List.flatten(list) end
+
+    assert_equiv.([])
+    assert_equiv.([1,2,3])
+
+    assert_equiv.([1,2,[3]])
+    assert_equiv.([1, [2, [2]], 3])
+    assert_equiv.([1, [2, [[2,2]]], 3])
+
+  end
 
 
 end
