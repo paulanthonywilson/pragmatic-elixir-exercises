2a2814474042b3d29be0c7106c607216bf461095
Paul Wilson <paulanthonywilson@gmail.com>
2013-12-30 11:49:54 +0000

    reads fixture

--- lib/pragtaxes.ex
+++ lib/pragtaxes.ex
@@ -1,8 +1,33 @@
 defmodule Pragtaxes do
-  def add_taxes taxrates, orders do 
+  def add_taxes taxrates, orders do
     orders |> Enum.map fn [id: id, ship_to: state, net_amount: amount] ->
       rate = Keyword.get taxrates, state, 0
       [id: id, ship_to: state, net_amount: amount, total_amount: amount + amount * rate]
     end
+  end
+
+  def decode_state state_string do
+    case state_string do
+      ":NC" -> :NC
+      ":OK" -> :OK
+      ":MA" -> :MA
+      ":CA" -> :CA
+      ":TX" -> :TX
+    end
+  end
+
+  def read_csv_line line do
+    [id, ship_to, net_amount] = line |> String.strip |> String.split(",")
+    [id: binary_to_integer(id), ship_to: decode_state(ship_to), net_amount: binary_to_float(net_amount)]
+  end
+
+
+  def process_file filename, tax_rates do
+    {:ok, records} = File.open filename, [:read], fn file ->
+      IO.read(file, :line) # discard first line
+      file |> IO.stream(:line)
+           |> Enum.map &read_csv_line(&1)
+    end
+    add_taxes(tax_rates, records)
   end
 end
--- test/pragtaxes_test.exs
+++ test/pragtaxes_test.exs
@@ -8,27 +8,51 @@ defmodule PragtaxesTest do
 
   test "adds taxes to TX and NC" do
     orders = [
-    [ id: 123, ship_to: :NC, net_amount: 100.00 ],
-    [ id: 125, ship_to: :TX, net_amount:  24.00 ],
-    ]
+      [ id: 123, ship_to: :NC, net_amount: 100.00 ],
+      [ id: 125, ship_to: :TX, net_amount:  24.00 ],
+      ]
     with_taxes = Pragtaxes.add_taxes(tax_rates, orders)
     assert with_taxes == [
-    [ id: 123, ship_to: :NC, net_amount: 100.00, total_amount: 107.50 ],
-    [ id: 125, ship_to: :TX, net_amount:  24.00, total_amount:  25.92],
-    ]
+      [ id: 123, ship_to: :NC, net_amount: 100.00, total_amount: 107.50 ],
+      [ id: 125, ship_to: :TX, net_amount:  24.00, total_amount:  25.92],
+      ]
 
-end
+  end
 
   test "states without taxes" do
     orders = [
-    [ id: 128, ship_to: :MA, net_amount:  10.00 ],
-    [ id: 129, ship_to: :CA, net_amount: 102.00 ],
-    ]
+      [ id: 128, ship_to: :MA, net_amount:  10.00 ],
+      [ id: 129, ship_to: :CA, net_amount: 102.00 ],
+      ]
     with_taxes = Pragtaxes.add_taxes(tax_rates, orders)
     assert with_taxes == [
-    [ id: 128, ship_to: :MA, net_amount:  10.00, total_amount: 10.00 ],
-    [ id: 129, ship_to: :CA, net_amount: 102.00, total_amount: 102.00 ],
-    ]
+      [ id: 128, ship_to: :MA, net_amount:  10.00, total_amount: 10.00 ],
+      [ id: 129, ship_to: :CA, net_amount: 102.00, total_amount: 102.00 ],
+      ]
+  end
+
+  test "processes the csv file" do
+    fixture_file = "#{Path.dirname(__FILE__)}/taxes_fixture.csv"
+    processed = Pragtaxes.process_file(fixture_file, tax_rates)
+    expected =
+    [
+      [ id: 123, ship_to: :NC, net_amount:  100.00, total_amount: 107.50 ],
+      [ id: 124, ship_to: :OK, net_amount:  35.50, total_amount: 35.5 ],
+      [ id: 125, ship_to: :TX, net_amount:  24.00, total_amount: 25.92 ],
+      [ id: 126, ship_to: :TX, net_amount:  44.80, total_amount: 48.384 ],
+      [ id: 127, ship_to: :NC, net_amount:  25.00, total_amount: 26.875 ],
+      [ id: 128, ship_to: :MA, net_amount:  10.00, total_amount: 10.00 ],
+      [ id: 129, ship_to: :CA, net_amount:  102.00, total_amount: 102.00 ],
+      [ id: 130, ship_to: :NC, net_amount:  50.00, total_amount: 53.75 ],
+      ]
+    List.zip([processed, expected]) |> Enum.each fn {l, r} ->
+      assert l == r
+    end
+    assert processed == expected
+  end
+
+  test "read csv line" do
+    assert Pragtaxes.read_csv_line("123,:NC,100.00") == [ id: 123, ship_to: :NC, net_amount:  100.00]
   end
 
 
